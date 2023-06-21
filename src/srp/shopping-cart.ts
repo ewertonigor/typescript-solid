3e322445c92129cc91e436792f009e068a81b7fc
Ewerton Igor <ewertonigor@icloud.com>
2023-06-21 12:50:51 -0300

    Conclusão do carrinho de compras back-end

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,8 +1,9 @@
 type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
 
 export class ShoppingCart {
   private readonly _items: CartItem[] = []; // -> Começando o array como vazio
-
+  private _orderStatus: OrderStatus = 'open';
   addItem(item: CartItem): void {
     this._items.push(item);
   }
@@ -14,6 +15,44 @@ export class ShoppingCart {
   get items(): Readonly<CartItem[]> {
     return this._items;
   }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items // o toFixed transforma em string, foi colocado o '+' para transformar em number
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+    }
+
+    this._orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido.`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada', msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo...');
+    this._items.length = 0;
+  }
 }
 
 const shoppingCart = new ShoppingCart();
@@ -21,6 +60,8 @@ shoppingCart.addItem({ name: 'Tênis', price: 699.99 });
 shoppingCart.addItem({ name: 'Moletom', price: 399.99 });
 shoppingCart.addItem({ name: 'Camiseta', price: 199.99 });
 
-shoppingCart.items[1] = { name: 'Sandalia', price: 89.99 };
-
 console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.orderStatus);
+shoppingCart.checkout();
+console.log(shoppingCart.orderStatus);
