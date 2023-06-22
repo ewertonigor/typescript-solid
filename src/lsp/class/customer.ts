8777f76814d02b98350b21417825e41b582c742d
Ewerton Igor <ewertonigor@icloud.com>
2023-06-22 19:07:00 -0300

    Interface Segregation Principle 1

---
+++ src/lsp/class/customer.ts
@@ -0,0 +1,26 @@
+import {
+  IndividualCustomerProtocol,
+  EnterpriseCustomerProtocol,
+} from './types/customer-protocol';
+
+export class IndividualCustomer implements IndividualCustomerProtocol {
+  firstName: string;
+  lastName: string;
+  cpf: string;
+
+  constructor(firstName: string, lastName: string, cpf: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.cpf = cpf;
+  }
+}
+
+export class EnterpriseCustomer implements EnterpriseCustomerProtocol {
+  name: string;
+  cnpj: string;
+
+  constructor(name: string, cnpj: string) {
+    this.cnpj = cnpj;
+    this.name = name;
+  }
+}
---
+++ src/lsp/class/types/customer-protocol.ts
@@ -0,0 +1,10 @@
+export type IndividualCustomerProtocol = {
+  firstName: string;
+  lastName: string;
+  cpf: string;
+};
+
+export type EnterpriseCustomerProtocol = {
+  name: string;
+  cnpj: string;
+};
